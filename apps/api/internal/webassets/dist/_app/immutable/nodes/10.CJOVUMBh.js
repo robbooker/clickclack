925105fac8fdcbde3790b0084e71987f68916133
Peter Steinberger <steipete@gmail.com>
2026-07-12 11:31:17 -0700

    build: refresh embedded switchboard web assets

---
+++ apps/api/internal/webassets/dist/_app/immutable/nodes/10.CJOVUMBh.js
@@ -0,0 +1,1 @@
+import{gt as e,ht as t}from"../chunks/DXS_Y5Rz.js";import"../chunks/xihTtKlq.js";import{t as n}from"../chunks/D4Jlw-Vk.js";function r(r,i){e(i,!0),n(r,{get routeWorkspaceID(){return i.params.workspaceID},get routeTargetID(){return i.params.targetID}}),t()}export{r as component};
